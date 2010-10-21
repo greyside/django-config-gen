b06db9288fadde93ac297c04bc86d9b78d9eccc0
Sean Hayes <sean@seanhayes.name>
2010-10-21 02:58:27 -0400

    can now overwrite directory locations in settings.py

--- management/commands/config_gen.py
+++ management/commands/config_gen.py
@@ -5,29 +5,33 @@ from django.template import Template, Context
 import os
 import logging
 
+CONFIG_DIR = settings.CONFIG_GEN_CONFIG_DIR if hasattr(settings, 'CONFIG_GEN_CONFIG_DIR') else 'config/'
+TEMPLATES_DIR = settings.CONFIG_GEN_TEMPLATES_DIR if hasattr(settings, 'CONFIG_GEN_TEMPLATES_DIR') else '%stemplates/' % CONFIG_DIR
+GENERATED_DIR = settings.CONFIG_GEN_GENERATED_DIR if hasattr(settings, 'CONFIG_GEN_GENERATED_DIR') else '%sgenerated/' % CONFIG_DIR
+
 class Command(NoArgsCommand):
 	help = 'Generates configuration files for Apache, Nginx, etc. using values in settings.py and the Django template system.'
 	
 	def handle_noargs(self, **options):
-		#get all templates in config/templates/, parse them, and output files in config/
+		#get all templates in TEMPLATES_DIR, parse them, and output files in GENERATED_DIR
 		#logging.debug(settings)
-		ctx = Context(settings._wrapped.__dict__)
-		config_dir = 'config/'
-		templates_dir = '%stemplates/' % config_dir
-		output_dir = '%sgenerated/' % config_dir
+		ctx = Context(dict(settings._wrapped))
 		
-		if not os.path.exists(templates_dir):
-			os.makedirs(templates_dir)
+		if not os.path.exists(CONFIG_DIR):
+			os.makedirs(CONFIG_DIR)
 		
-		if not os.path.exists(output_dir):
-			os.makedirs(output_dir)
+		if not os.path.exists(TEMPLATES_DIR):
+			os.makedirs(TEMPLATES_DIR)
 		
-		dir_list=os.listdir(templates_dir)
+		if not os.path.exists(GENERATED_DIR):
+			os.makedirs(GENERATED_DIR)
+		
+		dir_list=os.listdir(TEMPLATES_DIR)
 		for filename in dir_list:
-			fi = open('%s%s' % (templates_dir, filename), 'r')
+			fi = open('%s%s' % (TEMPLATES_DIR, filename), 'r')
 			t = Template(fi.read())
 			fi.close()
 			
-			fo = open('%s%s' % (output_dir, filename), 'w')
+			fo = open('%s%s' % (GENERATED_DIR, filename), 'w')
 			fo.write(t.render(ctx))
 			fo.close()

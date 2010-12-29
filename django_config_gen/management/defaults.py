27f778901248c2e94f0380c3c3919f7211dcc103
Sean Hayes <sean@seanhayes.name>
2010-12-28 22:21:23 -0500

    added manifest template to overcome setup.py bug, fixed bug in defaults.py

--- django_config_gen/management/defaults.py
+++ django_config_gen/management/defaults.py
@@ -9,4 +9,8 @@ _config_dir = os.path.join(PROJECT_ROOT ,'config')
 TEMPLATES_DIR = os.path.join(_config_dir ,'templates')
 GENERATED_DIR = os.path.join(_config_dir ,'generated')
 
-HOST = Site.objects.get_current().domain.split(':')[0]
+#Error is thrown when running "./manage.py syncdb" for the first time and this app is installed.
+try:
+	HOST = Site.objects.get_current().domain.split(':')[0]
+except:
+	HOST = 'localhost'

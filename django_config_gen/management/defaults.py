2cc2a09f4c28d4110191029d871c62f78b5ef6e7
Sean Hayes <sean@seanhayes.name>
2010-12-28 23:09:31 -0500

    maybe this will work

--- django_config_gen/management/defaults.py
+++ django_config_gen/management/defaults.py
@@ -1,6 +1,7 @@
 import __main__
 import os
 from django.contrib.sites.models import Site
+from django.db import transaction
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__main__.__file__))
 LOG_DIR = os.path.join(PROJECT_ROOT ,'logs')
@@ -10,8 +11,9 @@ TEMPLATES_DIR = os.path.join(_config_dir ,'templates')
 GENERATED_DIR = os.path.join(_config_dir ,'generated')
 
 #Error is thrown when running "./manage.py syncdb" for the first time and this app is installed.
+try:
+	HOST = Site.objects.get_current().domain.split(':')[0]
+except:
+	transaction.rollback()
+	HOST = 'localhost'
 
-def _get_host():
-	return Site.objects.get_current().domain.split(':')[0]
-
-HOST = property(_get_host)
